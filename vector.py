b5b0618814a31c0d38f84a21babb1be6a279862b
furkanonder <furkantahaonder@gmail.com>
2019-07-03 00:01:10 +0300

    new functions added

--- vector.py
+++ vector.py
@@ -9,3 +9,9 @@ class Vector:
 
     def length(self):
         return math.sqrt(self.X*self.X + self.Y*self.Y + self.Z*self.Z)      
+
+    def multiplyScalar(self,t):
+        return Vector(self.X*t, self.Y*t,self.Z*t)
+
+    def add(self,o):
+        return Vector(self.X+o.X, self.Y+o.Y,self.Z+o.Z)
