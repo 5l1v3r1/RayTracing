5557d62985366d0f53debd262aee9c3ce7818cc6
furkanonder <furkantahaonder@gmail.com>
2019-07-03 00:01:36 +0300

    ray added

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from vector import Vector
+from ray import Ray
 
 nx = 200
 ny = 100
@@ -10,13 +11,30 @@ const = 255.99
 #nx pixel by ny pixel image
 print(f"P3\n{nx} {ny}\n255")
 
+lowerLeft = Vector(-2.0, -1.0, -1.0)
+horizontal = Vector(4.0, 0.0, 0.0)
+vertical = Vector(0.0, 2.0, 0.0)
+origin = Vector(0.0, 0.0, 0.0)
+
 j = ny-1
-while(j>=0):
-	for i in range(nx):
-		# red and green values range from 0.0 to 1.0
-		v = Vector(float(i/nx),float(j/ny),0.2)
-		ir = int(const*v.X)
-		ig = int(const*v.Y)
-		ib = int(const*v.Z)
-		print(ir, ig, ib)       
-	j-=1   
+
+while(j >= 0):
+    for i in range(nx):
+        u = float(i/nx)
+        v = float(j/ny)
+    
+        hor = horizontal.multiplyScalar(u)
+        vert = vertical.multiplyScalar(v)
+
+        v1 = hor.add(vert)
+        v2 = lowerLeft.add(v1)
+        direction = v1.add(v2)
+
+        r = Ray(origin, direction)
+        rgb = r.color(r)
+
+        ir = int(const*rgb.X)
+        ig = int(const*rgb.Y)
+        ib = int(const*rgb.Z)
+        print(ir, ig, ib)       
+    j-=1
