758df00aa37419f00441a01453106c6da4c8500c
furkanonder <furkantahaonder@gmail.com>
2019-07-07 16:53:56 +0300

    functions refactored

--- ray.py
+++ ray.py
@@ -11,17 +11,17 @@ class Ray:
 
     def pointAtParameter(self,t):
         return self.a + self.b*multiplyScalar(t)
-    
 
+       
     def hit_sphere(self,center,radius,r):
-        oc = r.origin.subtract(center)
+        oc = r.origin - center
         a = r.direction.dot(r.direction)
-        b = oc.multiplyScalar(2.0).dot(r.direction)
-        c = oc.dot(oc).subtractScalar(radius*radius)
-        discriminant = b.multiply(b).subtract(a.multiply(c).multiplyScalar(-4))
-        
-        return (discriminant.X + discriminant.Y + discriminant.Z ) > 0
-   
+        b = 2 * r.direction.dot(oc)
+        c = oc.dot(oc) - (radius*radius)
+        discriminant = b*b - 4*a*c
+
+        return discriminant > 0        
+    
 
     def color(self):
         r = self.hit_sphere(Vector(0.0, 0.0, -1.0), 0.5, self)
@@ -30,8 +30,8 @@ class Ray:
             return Vector(1.0, 0.0, 0.0)
 
         unitDirection = self.direction.normalize()
-        t = 0.5 * (unitDirection.Y + 1.0)
+        t = 0.5 * (unitDirection.y + 1.0)
         white = Vector(1.0, 1.0, 1.0)
         blue = Vector(0.5, 0.7, 1.0)
 
-        return white.multiplyScalar(1.0 - t).add(blue.multiplyScalar(t))
+        return white.multiplyScalar(1.0 - t) + blue.multiplyScalar(t)
--- vector.py
+++ vector.py
@@ -1,33 +1,35 @@
 import math
 
+
 class Vector:
-	
-    def __init__(self,X,Y,Z):
-        self.X = X
-        self.Y = Y
-        self.Z = Z
+    
+    def __init__(self, x = 0.0, y = 0.0, z = 0.0):
+        self.x = x
+        self.y = y
+        self.z = z
+
 
     def length(self):
-        return math.sqrt(self.X*self.X + self.Y*self.Y + self.Z*self.Z)      
+        return math.sqrt(self.x * self.x + self.y * self.y + self.z * self.z)      
 
     def multiplyScalar(self,t):
-        return Vector(self.X*t, self.Y*t,self.Z*t)
+        return Vector(self.x * t, self.y * t, self.z * t)
 
-    def multiply(self,t):
-        return Vector(self.X*t.X, self.Y*t.Y,self.Z*t.Z)
+    def __mul__(self,t):
+        return Vector(self.x * t.x, self.y * t.y, self.z * t.z)
 
-    def add(self,o):
-        return Vector(self.X+o.X, self.Y+o.Y,self.Z+o.Z)
+    def __add__(self,o):
+        return __class__(self.x + o.x, self.y + o.y,self.z + o.z)
 
     def subtractScalar(self,t):
-        return Vector(self.X - t, self.Y - t, self.Z - t)
+        return Vector(self.x - t, self.y - t, self.z - t)
 
-    def subtract(self,o):
-        return Vector(self.X - o.X, self.Y - o.Y, self.Z - o.Z)
+    def __sub__(self,o):
+        return __class__(self.x - o.x, self.y - o.y, self.z - o.z)
     
     def dot(self,o):
-        return Vector(self.X * o.X, self.Y * o.Y, self.Z * o.Z)
+        return self.x * o.x + self.y * o.y + self.z * o.z
 
     def normalize(self):
         l = self.length()
-        return Vector(self.X / l, self.Y / l, self.Z / l)
+        return Vector(self.x / l, self.y / l, self.z / l)
